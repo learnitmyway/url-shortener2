64249b9ccc2eec9486455aacf9a75273a817d98b
David <learnitmyway@users.noreply.github.com>
2023-11-27 17:01:41 +0100

    initializse with Spring Boot

---
+++ src/main/kotlin/com/learnitmyway/urlshortener2/UrlShortener2Application.kt
@@ -0,0 +1,11 @@
+package com.learnitmyway.urlshortener2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UrlShortener2Application
+
+fun main(args: Array<String>) {
+	runApplication<UrlShortener2Application>(*args)
+}
---
+++ src/test/kotlin/com/learnitmyway/urlshortener2/UrlShortener2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.learnitmyway.urlshortener2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UrlShortener2ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
