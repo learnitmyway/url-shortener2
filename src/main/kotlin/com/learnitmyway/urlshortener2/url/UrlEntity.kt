339660a1ddd1c73345c1dffd47fb329957368a96
David <learnitmyway@users.noreply.github.com>
2023-12-04 10:53:06 +0100

    remove trailing comma

--- src/main/kotlin/com/learnitmyway/urlshortener2/url/UrlEntity.kt
+++ src/main/kotlin/com/learnitmyway/urlshortener2/url/UrlEntity.kt
@@ -4,4 +4,4 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document("urls")
-data class UrlEntity(@Id val key: String, val longUrl: String, )
+data class UrlEntity(@Id val key: String, val longUrl: String)
